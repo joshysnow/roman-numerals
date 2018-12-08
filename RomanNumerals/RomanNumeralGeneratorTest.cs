0db9d94ce61cb206303fc127b754af40cfc0b1b0
Joshua Hirst <hirst.joshua@me.com>
2018-12-08 13:32:28 +0000

    Translate 0-9 to roman numerals

--- RomanNumerals/RomanNumeralGeneratorTest.cs
+++ RomanNumerals/RomanNumeralGeneratorTest.cs
@@ -9,10 +9,73 @@ namespace RomanNumerals
         private RomanNumeralGenerator _generator = new RomanNumeralGenerator();
 
         [Test()]
+        public void Translate_0()
+        {
+            string result = _generator.Generate(0);
+            Assert.AreEqual(string.Empty, result);
+        }
+
+        [Test()]
         public void Translate_1_I()
         {
             string result = _generator.Generate(1);
             Assert.AreEqual("I", result);
+        }
+
+        [Test()]
+        public void Translate_2_II()
+        {
+            string result = _generator.Generate(2);
+            Assert.AreEqual("II", result);
+        }
+
+        [Test()]
+        public void Translate_3_III()
+        {
+            string result = _generator.Generate(3);
+            Assert.AreEqual("III", result);
+        }
+
+        [Test()]
+        public void Translate_4_IV()
+        {
+            string result = _generator.Generate(4);
+            Assert.AreEqual("IV", result);
+        }
+
+        [Test()]
+        public void Translate_5_V()
+        {
+            string result = _generator.Generate(5);
+            Assert.AreEqual("V", result);
+        }
+
+        [Test()]
+        public void Translate_6_VI()
+        {
+            string result = _generator.Generate(6);
+            Assert.AreEqual("VI", result);
+        }
+
+        [Test()]
+        public void Translate_7_VII()
+        {
+            string result = _generator.Generate(7);
+            Assert.AreEqual("VII", result);
+        }
+
+        [Test()]
+        public void Translate_8_VIII()
+        {
+            string result = _generator.Generate(8);
+            Assert.AreEqual("VIII", result);
+        }
+
+        [Test()]
+        public void Translate_9_IX()
+        {
+            string result = _generator.Generate(9);
+            Assert.AreEqual("IX", result);
         }
     }
 }
