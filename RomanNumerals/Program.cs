fdf0cf11d918d8c3ed15c22c31dea24c3724fa9c
Joshua Hirst <hirst.joshua@me.com>
2018-12-08 16:33:11 +0000

    Added a simple console app to accept numbers and output them in roman numerals

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -6,7 +6,38 @@ namespace RomanNumerals
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Roman Numeral Generator program!\n");
+            Console.WriteLine("Enter a base10 number to translate it into Roman numerals!");
+            Console.WriteLine("\nEnter Q to quit\n");
+
+            RomanNumeralGenerator generator = new RomanNumeralGenerator();
+            string input;
+            string output;
+            bool end = false;
+
+            do
+            {
+                Console.Write("Enter: ");
+                input = Console.ReadLine();
+
+                if (input.ToLower().Contains("q"))
+                    break;
+
+                bool parsed = int.TryParse(input, out int number);
+
+                if (int.TryParse(input, out number))
+                {
+                    output = generator.Generate(number);
+                    Console.WriteLine(" = " + output + "\n");
+                }
+                else
+                {
+                    Console.WriteLine("\nProblem reading number, please try again...\n");
+                }
+
+            } while (!end);
+
+            Console.WriteLine("\nGoodbye!\n");
         }
     }
 }
